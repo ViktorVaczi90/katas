8441360f257cb4fd2341dc638cee3294eafb7ab6
Viktor Vaczi <viktor.vaczi.90@gmail.com>
2017-07-13 17:19:03 +0200

    added Functional SQL

---
+++ Functional SQL.js
@@ -0,0 +1,14 @@
+/**
+ * Created by v on 7/13/17.
+ */
+var query = function() {
+  return{select, from, where, orderBy, groupBy, having, execute}
+};
+function select(propety){}
+function from(data){}
+function where(filter){}
+function orderBy(order){}
+function groupBy(group){}
+function having(expression){}
+function execute(){}
+console.log(query())
