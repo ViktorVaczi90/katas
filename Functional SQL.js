79b4a0815a3802c38037db9de03e56565c4c12ae
Viktor Vaczi <viktor.vaczi.90@gmail.com>
2017-07-13 19:10:31 +0200

    before recursive grouping function

--- Functional SQL.js
+++ Functional SQL.js
@@ -1,14 +1,56 @@
-/**
- * Created by v on 7/13/17.
- */
 var query = function() {
-  return{select, from, where, orderBy, groupBy, having, execute}
+  return{data: [], propety: i=>i, filter: i=>true, group: undefined,
+    error:false,
+    select, from, where, orderBy, groupBy, having, execute}
 };
-function select(propety){}
-function from(data){}
-function where(filter){}
-function orderBy(order){}
-function groupBy(group){}
-function having(expression){}
-function execute(){}
-console.log(query())
+function generateErrorFunction(errorStr){
+  return function(){
+    throw new Error(errorStr)
+  }
+}
+function groupData(groupingFunctions = [], data){
+
+}
+function select(propety){return Object.assign({},this, {propety: propety?propety:this.propety, select: generateErrorFunction.bind(this)('Duplicate SELECT')})}
+function from(data){return Object.assign({},this, {data, from: generateErrorFunction.bind(this)('Duplicate FROM')})}
+function where(filter){return Object.assign({},this, {filter: filter?filter:this.filter, where: generateErrorFunction.bind(this)('Duplicate WHERE')})}
+function orderBy(order){return Object.assign({},this, {order, orderBy: generateErrorFunction.bind(this)('Duplicate ORDERBY')})}
+function groupBy(...group){return Object.assign({},this, {group: group?group:this.group, groupBy: generateErrorFunction.bind(this)('Duplicate GROUPBY')})}
+function having(expression){return Object.assign({},this, {expression, having: generateErrorFunction.bind(this)('having Error')})}
+function execute(){
+  return this.data
+    .filter(this.filter)
+    .reduce((acc,curr)=>{
+      if(!this.group) return acc.concat([curr])
+      acc.filter(i=>i[0]===this.group(curr)).length
+      ?acc.filter(i=>i[0]===this.group(curr))[0][1].push(curr)
+      :acc.push([this.group(curr),[curr]])
+    return acc},[])
+    .map(this.propety)}
+
+var persons = [
+  {name: 'Peter', profession: 'teacher', age: 20, maritalStatus: 'married'},
+  {name: 'Michael', profession: 'teacher', age: 50, maritalStatus: 'single'},
+  {name: 'Peter', profession: 'teacher', age: 20, maritalStatus: 'married'},
+  {name: 'Anna', profession: 'scientific', age: 20, maritalStatus: 'married'},
+  {name: 'Rose', profession: 'scientific', age: 50, maritalStatus: 'married'},
+  {name: 'Anna', profession: 'scientific', age: 20, maritalStatus: 'single'},
+  {name: 'Anna', profession: 'politician', age: 50, maritalStatus: 'married'}
+];
+function profession(person) {
+  return person.profession;
+}
+function name(person) {
+  return person.name;
+}
+function isTeacher(person) {
+  return person.profession === 'teacher';
+}
+let retval = query()
+//retval = retval.select(profession)
+//retval = retval.select(profession)
+retval = retval.from(persons)
+//retval = retval.where(isTeacher)
+retval = retval.groupBy(profession)
+retval = retval.execute()
+console.log(retval)
